06984576d288ca2d8759ce8e5d471933dc118d9a
Soham Dutta <123827172+123456788940@users.noreply.github.com>
2024-01-16 13:18:18 +0530

    Create simpleMultiSig.sol

---
+++ simpleMultiSig.sol
@@ -0,0 +1,46 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract multiSig is ERC20 {
+    address public owner;
+
+    constructor() ERC20("Shiva", "Sh") {
+        owner = msg.sender;
+    }
+
+    struct wallet {
+        uint _amount;
+        bool sent;
+        bool received;
+    }
+    mapping(address=>wallet) public Wallet;
+
+address[] public owners = [0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB];
+
+    function send(address to, uint amount, address[] memory _owners) external payable{
+        _owners = owners;
+        require(_owners.length ==2, "not the sufficient number of owners");
+        for (uint i = 0; i < _owners.length; i++) {
+           require(Wallet[_owners[i]].sent, "amount not sent");
+           Wallet[_owners[i]].sent = true;
+           _burn(to, amount);
+        }
+    }
+
+
+    function _receive(uint amount, address to, address[] memory _owners) external {
+        _owners = owners;
+
+          require(_owners.length ==2, "not the sufficient number of owners");
+          for (uint i = 0; i < _owners.length; i++) {
+           require(Wallet[_owners[i]].sent, "amount not sent");
+           Wallet[_owners[i]].received = true;
+           _mint(to, amount);
+        }
+    }
+
+    
+}
